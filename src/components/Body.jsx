95f00c1fecd0861227226e99a0558fd2a59249af
esitorsunil <sunilrio23@gmail.com>
2025-03-24 21:47:16 +0530

    login auth using navigate

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import NavBar from './NavBar'
-import { Outlet } from 'react-router'
+import { Outlet, useNavigate } from 'react-router'
 import Footer from './Footer'
+import { BASE_URL } from '../utils/constants'
+import { useDispatch } from 'react-redux'
+import { addUser } from '../utils/userSlice'
 
 const Body = () => {
+   const dispatch = useDispatch();
+   const navigate = useNavigate();
+   const userData = useSelector((store) => store.user);
+  const fetchUser = async() => {
+    if(userData) return;
+    try{
+    const res = await axios.get(BASE_URL + "/profile/view", {
+      withCredentials: true,
+    }); 
+    dispatch(addUser(res.data));
+  }
+    catch(err) {
+      if(err.status === 401){
+      navigate("/login");
+      }
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    fetchUser();
+  }, [])
   return (
     <div>
         <NavBar/>
